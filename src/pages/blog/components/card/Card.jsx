e1f3bebee2172542c51abce698a8019edd1204fb
maheshbasnet089 <basnetmanish089@gmail.com>
2024-04-14 19:23:12 +0545

    complete page UI

--- src/pages/blog/components/card/Card.jsx
+++ src/pages/blog/components/card/Card.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Card = () => {
+  
   return (
-    <div class="max-w-sm rounded overflow-hidden shadow-lg mb-4">
+  <Link to="/blog/id">
+      <div class="max-w-sm rounded overflow-hidden shadow-lg mb-4">
     <img class="w-full" src="https://images.emojiterra.com/google/android-12l/512px/1f9d9-2642.png" alt="Sunset in the mountains" />
     <div class="px-6 py-4">
       <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
@@ -16,6 +19,7 @@ const Card = () => {
       <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
     </div>
   </div>
+  </Link>
   )
 }
 
