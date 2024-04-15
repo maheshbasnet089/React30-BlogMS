b6e0eb211a6f11446a13ba89b52fe115cb20ca04
maheshbasnet089 <basnetmanish089@gmail.com>
2024-04-15 19:44:36 +0545

    add register,login

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import Form from './components/form/Form'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 const Login = () => {
+
+  const navigate = useNavigate()
+  const handleLogin = async (data)=>{
+   try {
+    const response = await axios.post("https://react30.onrender.com/api/user/login",data)
+   if(response.status === 200){
+    navigate('/')
+   }else{
+    alert("Login failed")
+   }
+   } catch (error) {
+    alert(error?.response?.data?.message)
+   }
+  }
+
+
   return (
-   
-  <Form type='Login' />
+  <Form type='Login' onSubmit={handleLogin} />
   )
 }
 
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import Form from './components/form/Form'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
-const Register = () => {
+const Register =  () => {
+  const navigate = useNavigate()
+  const handleRegister = async (data)=>{
+   try {
+    const response = await axios.post("https://react30.onrender.com/api/user/register",data)
+   if(response.status === 201){
+    navigate('/login')
+   }else{
+    alert("Registration failed")
+   }
+   } catch (error) {
+    alert(error?.response?.data?.message)
+   }
+  }
   return (
-    <Form type='Register'/>
+    <Form type='Register' onSubmit={handleRegister} />
   )
 }
 
