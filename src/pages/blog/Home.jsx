e366d83d5b13f3f1d16aa80c6eb8943da1358acf
maheshbasnet089 <basnetmanish089@gmail.com>
2024-04-19 19:31:45 +0545

    implement all blog

--- src/pages/blog/Home.jsx
+++ src/pages/blog/Home.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Layout from '../../components/layout/Layout'
 import Card from './components/card/Card'
+import axios from 'axios'
+import { baseUrl } from '../../config'
 
 const Home = () => {
+  const [blogs,setBlogs] = useState([])
+  const fetchBlogs = async ()=>{
+    const response = await axios.get(`${baseUrl}/blog`)
+    if(response.status === 200){
+      setBlogs(response.data.data)
+    }
+  }
+  useEffect(()=>{
+    fetchBlogs()
+  },[])
+
   return (
    <Layout>
   <div className='flex flex-wrap justify-center space-x-5 mt-6'>
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+    {
+      blogs.length > 0 && blogs.map((blog)=>{
+        console.log(blog)
+        return (
+          <Card blog={blog} />
+        )
+      })
+    }
+  
 
   </div>
    </Layout>
--- src/pages/blog/components/card/Card.jsx
+++ src/pages/blog/components/card/Card.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Card = () => {
+const Card = ({blog}) => {
   
   return (
-  <Link to="/blog/id">
+  <Link to={`/blog/${blog._id}`}>
       <div class="max-w-sm rounded overflow-hidden shadow-lg mb-4">
-    <img class="w-full" src="https://images.emojiterra.com/google/android-12l/512px/1f9d9-2642.png" alt="Sunset in the mountains" />
+    <img class="w-full" src={blog.imageUrl} alt="Sunset in the mountains" />
     <div class="px-6 py-4">
-      <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+      <div class="font-bold text-xl mb-2">{blog.title}</div>
       <p class="text-gray-700 text-base">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+      {blog.description}
       </p>
     </div>
     <div class="px-6 pt-4 pb-2">
-      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{blog.category}</span>
+
     </div>
   </div>
   </Link>
