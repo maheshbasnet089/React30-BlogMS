016ab8975a708e50f3cbb75faae1a36d9b1d09b9
maheshbasnet089 <basnetmanish089@gmail.com>
2024-04-21 19:27:57 +0545

    add delete, single blog

--- src/pages/blog/SingleBlog.jsx
+++ src/pages/blog/SingleBlog.jsx
@@ -1,55 +1,81 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../../components/layout/Layout'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import axios from 'axios'
+import { baseUrl } from '../../config'
 
 const SingleBlog = () => {
-    
+    const {id} = useParams()
+    const [blog,setBlog] = useState({})
+    const navigate = useNavigate()
+
+    const deleteBlog = async ()=>{
+      try {
+        const response = await axios.delete(`${baseUrl}/blog/${id}`,{
+            headers : {
+                'Authorization' : localStorage.getItem('token')
+            }
+        })
+        if(response.status === 200){
+          navigate('/')
+        }else{
+            alert("Something went wrong. Try again !")
+        }
+      } catch (error) {
+        alert(error.response.data.message)
+      }
+    }
+
+    const fetchBlog = async ()=>{
+      const response = await axios.get(`${baseUrl}/blog/${id}`)
+      if(response.status === 200){
+        setBlog(response.data.data)
+      }
+    }
+    useEffect(()=>{
+      fetchBlog()
+    },[])
   return (
     <Layout>
 
-<div class="bg-gray-100 dark:bg-gray-800 py-8 h-screen">
-    <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex flex-col md:flex-row -mx-4">
-            <div class="md:flex-1 px-4">
-                <div class="h-[460px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
-                    <img class="w-full h-full object-cover" src="https://cdn.pixabay.com/photo/2020/05/22/17/53/mockup-5206355_960_720.jpg" alt="Product Image" />
+<div className="bg-gray-100 dark:bg-gray-800 py-8 h-screen">
+    <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-col md:flex-row -mx-4">
+            <div className="md:flex-1 px-4">
+                <div className="h-[460px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
+                    <img className="w-full h-full object-cover" src={blog?.imageUrl} alt="Product Image" />
                 </div>
-                <div class="flex -mx-2 mb-4">
-                    <div class="w-1/2 px-2">
+                <div className="flex -mx-2 mb-4">
+                    <div className="w-1/2 px-2">
                         <Link to='/blog/edit'>
-                        <button class="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">Edit</button> 
+                        <button className="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">Edit</button> 
                         </Link>
                     </div>
-                    <div class="w-1/2 px-2">
-                        <button class="w-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600">Delete</button>
+                    <div className="w-1/2 px-2">
+                        <button className="w-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600" onClick={deleteBlog} >Delete</button>
                     </div>
                 </div>
             </div>
-            <div class="md:flex-1 px-4">
-                <h2 class="text-2xl font-bold text-gray-800 dark:text-white mb-2">Blog Title</h2>
-                <p class="text-gray-600 dark:text-gray-300 text-sm mb-4">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed
-                    ante justo. Integer euismod libero id mauris malesuada tincidunt.
+            <div className="md:flex-1 px-4">
+                <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-2">{blog?.title}</h2>
+                <p className="text-gray-600 dark:text-gray-300 text-sm mb-4">
+                    {blog.subtitle}
                 </p>
-                <div class="flex mb-4">
-                    <div class="mr-4">
-                        <span class="font-bold text-gray-700 dark:text-gray-300">Category:</span>
-                        <span class="text-gray-600 dark:text-gray-300">$29.99</span>
+                <div className="flex mb-4">
+                    <div className="mr-4">
+                        <span className="font-bold text-gray-700 dark:text-gray-300">Category:</span>
+                        <span className="text-gray-600 dark:text-gray-300">${blog?.category}</span>
                     </div>
                     <div>
-                        <span class="font-bold text-gray-700 dark:text-gray-300">Published At:</span>
-                        <span class="text-gray-600 dark:text-gray-300">In Stock</span>
+                        <span className="font-bold text-gray-700 dark:text-gray-300">Author:  {blog?.userId?.username}</span>
+                 
                     </div>
                 </div>
  
                 <div>
-                    <span class="font-bold text-gray-700 dark:text-gray-300">Product Description:</span>
-                    <p class="text-gray-600 dark:text-gray-300 text-sm mt-2">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                        sed ante justo. Integer euismod libero id mauris malesuada tincidunt. Vivamus commodo nulla ut
-                        lorem rhoncus aliquet. Duis dapibus augue vel ipsum pretium, et venenatis sem blandit. Quisque
-                        ut erat vitae nisi ultrices placerat non eget velit. Integer ornare mi sed ipsum lacinia, non
-                        sagittis mauris blandit. Morbi fermentum libero vel nisl suscipit, nec tincidunt mi consectetur.
+                    <span className="font-bold text-gray-700 dark:text-gray-300">Product Description:</span>
+                    <p className="text-gray-600 dark:text-gray-300 text-sm mt-2">
+                       {blog?.description}
                     </p>
                 </div>
             </div>
